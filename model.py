20a2b77c3a6ddf9bb99b908c6254cc2d4bdab996
chalonhutson <chalonhutson@gmail.com>
2021-09-27 12:19:18 -0600

    Updated some of the classes for postgres tables.

--- model.py
+++ model.py
@@ -48,9 +48,15 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
-    movie_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     score = db.Column(db.Integer)
+
+    #Define the relationship to user
+    user = db.relationship("User", backref=db.backref("ratings", order_by=rating_id))
+
+    #Define the relationship to movie
+    movie = db.relationship("Movie", backref=db.backref("ratings", order_by=rating_id))
 
 
 ##############################################################################
