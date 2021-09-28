ad5f58197ef2a7c869dd3fccc9aece5a5e9c1681
chalonhutson <chalonhutson@gmail.com>
2021-09-27 20:39:22 -0600

    Added register functionality.

--- server.py
+++ server.py
@@ -2,13 +2,16 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
+
+
 
 
 app = Flask(__name__)
+
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
@@ -22,7 +25,50 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register", methods = ["POST"])
+def register_user():
+    email = request.form["email"]
+    password = request.form["password"]
+    password_confirm = request.form["password-confirm"]
+    age = request.form["age"]
+    zipcode = request.form["zipcode"]
+
+    if password != password_confirm:
+        flash("RIP, your passwords don't match.")
+        return render_template("homepage.html")
+    else:
+        user = User(email = email, password = password, age = age, zipcode = zipcode)
+        db.session.add(user)
+        db.session.commit()
+        flash("You added a user.")
+        return render_template("homepage.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    if User.query.filter_by(email=email):
+        query = User.query.filter_by(email = email)
+        results = query.first()
+        if results.password == password:
+            flash("Login successful.")
+            return render_template("homepage.html")
+        else:
+            flash("Incorrect password.")
+            return render_template("homepage.html")
+    else:
+        flash("Incorrect username.")
+        return render_template("homepage.html")
+
+
 
 
 if __name__ == "__main__":
